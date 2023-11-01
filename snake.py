b3d6be31bba20a29f03cb3eedb3df9c0ea5a7e0e
T03565 <victor.machado@sofist.co>
2023-11-01 19:05:28 -0300

    Changes on snake movement

--- snake.py
+++ snake.py
@@ -1,20 +1,24 @@
 import random
 import operator
+from pytimedinput import timedInput
+import os
 
 
 def update_snake():
-    print(snake[0])
-    direction = input()
-    if direction == "w":
-        move_to = DIRECTIONS["up"]
-    elif direction == "s":
-        move_to = DIRECTIONS["down"]
-    elif direction == "a":
-        move_to = DIRECTIONS["left"]
-    elif direction == "d":
-        move_to = DIRECTIONS["right"]
-    snake[0] = tuple(map(operator.add, snake[0], move_to))
-    print(snake[0])
+    direction = DIRECTIONS["left"]
+    txt, time = timedInput("", timeout=0.3)
+    match txt:
+        case "w":
+            direction = DIRECTIONS["up"]
+        case "s":
+            direction = DIRECTIONS["down"]
+        case "a":
+            direction = DIRECTIONS["left"]
+        case "d":
+            direction = DIRECTIONS["right"]
+    new_head = snake[0][0] + direction[0], snake[0][1] + direction[1]
+    snake.insert(0, new_head)
+    snake.pop(-1)
 
 
 def print_field():
@@ -44,10 +48,12 @@ snake = [
     (FIELD_WIDTH // 2 + 1, FIELD_HEIGHT // 2),
 ]
 
-DIRECTIONS = {"left": (-1, 0), "right": (1, 0), "up": (0, 1), "down": (0, -1)}
+DIRECTIONS = {"left": (-1, 0), "right": (1, 0), "up": (0, -1), "down": (0, 1)}
 
 # Apple
 apple_pos = (random.randrange(0, 31), random.randrange(0, 15))
 
-print_field()
-update_snake()
+while True:
+    os.system("cls")
+    print_field()
+    update_snake()
--- snake_practice.py
+++ snake_practice.py
@@ -1,26 +1,28 @@
 import random
+import operator
+from pytimedinput import timedInput
 
 
-def move_snake():
-    direction = input()
-    if direction == "w":
-        move_to = DIRECTIONS["up"]
-    elif direction == "s":
-        move_to = DIRECTIONS["down"]
-    elif direction == "a":
-        move_to = DIRECTIONS["left"]
-    elif direction == "d":
-        move_to = DIRECTIONS["right"]
-    return move_to
+def controls():
+    control = input()
+    if control == "w":
+        direction = DIRECTIONS["up"]
+    elif control == "s":
+        direction = DIRECTIONS["down"]
+    elif control == "a":
+        direction = DIRECTIONS["left"]
+    elif control == "d":
+        direction = DIRECTIONS["right"]
+    return direction
 
 
-def define_snake_position():
+def create_snake():
     snake_x = SCREEN_WIDTH // 2
     snake_y = SCREEN_HEIGHT // 2
     return [(snake_x + 3, snake_y), (snake_x + 2, snake_y), (snake_x + 1, snake_y)]
 
 
-def define_apple_position():
+def create_apple():
     apple_x = random.randrange(1, SCREEN_WIDTH - 1)
     apple_y = random.randrange(1, SCREEN_HEIGHT - 1)
     return (apple_x, apple_y)
@@ -28,9 +30,9 @@ def define_apple_position():
 
 def generate_screen():
     for cell in SCREEN:
-        if cell in snake_position:
+        if cell in snake:
             print("X", end="")
-        elif apple_position == cell:
+        elif apple == cell:
             print("@", end="")
         elif cell[0] in (0, SCREEN_WIDTH - 1) or cell[1] in (0, SCREEN_HEIGHT - 1):
             print("#", end="")
@@ -40,13 +42,22 @@ def generate_screen():
             print("", end="\n")
 
 
+def move_snake():
+    direction = controls()
+    snake[2] = snake[1]
+    snake[1] = snake[0]
+    snake[0] = tuple(map(operator.add, snake[0], direction))
+
+
 SCREEN_WIDTH = 32
 SCREEN_HEIGHT = 16
 SCREEN = [(col, row) for row in range(SCREEN_HEIGHT) for col in range(SCREEN_WIDTH)]
-DIRECTIONS = {"left": (-1, 0), "right": (1, 0), "up": (0, 1), "down": (0, -1)}
+DIRECTIONS = {"left": (-1, 0), "right": (1, 0), "up": (0, -1), "down": (0, 1)}
 
-snake_position = define_snake_position()
+snake = create_snake()
+apple = create_apple()
 
-apple_position = define_apple_position()
-
-generate_screen()
+while True:
+    generate_screen()
+    txt, time = timedInput("", timeout=0.3)
+    move_snake()
